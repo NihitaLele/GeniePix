f796695a38d018ab9616224237517c27dce8139c
NihitaLele <nihitalele@gmail.com>
2025-05-12 14:21:14 +0530

    bugs resolved

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://localhost/api/v1/post', {
+      const response = await fetch('https://geniepix.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
